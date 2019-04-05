0e749f8c23bc8544c42640a36a4273eceeb7ec3e
Jael Dubey <jael.dubey@heig-vd.ch>
2019-04-05 15:24:14 +0200

    Test de Travis

--- src/test/java/MonopolyGameTest.java
+++ src/test/java/MonopolyGameTest.java
@@ -1,5 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MonopolyGameTest {
+
+    @Test
+    void testIfTravisIsOk() {
+        assertTrue(true);
+    }
 
 }
