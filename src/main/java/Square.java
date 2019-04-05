f608bfe1a2af06d4ffe8019d4176f46a0b42ec78
X-Brast <alx-brast@outlook.com>
2019-04-05 16:03:41 +0200

    Mis en place de la classe Square avec son test

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,2 +1,18 @@
 public class Square {
+
+    private String name;
+    private int pos;
+
+    public Square(String name, int pos){
+        this.name = name;
+        this.pos = pos;
+    }
+
+    public int getLocation(){
+        return pos;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
