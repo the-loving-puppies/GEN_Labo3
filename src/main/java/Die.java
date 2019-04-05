24d7e4b2cf60b9fc68814c6147fd044fa5a5f185
Tatallias <jorge.fulgencioesteves@heig-vd.ch>
2019-04-05 16:15:50 +0200

    Implementation de Die et un test

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,2 +1,28 @@
+import java.util.Random;
+
 public class Die {
+	private int faceValue; // the value on the face side of the die
+	private Random rand; // random nmber generator
+	
+	public Die() {
+		faceValue = 0;
+		rand = new Random();
+	}
+	
+	/**
+	 * Rolls the die, sets the face value to a new number from 1 to 6
+	 */
+	public void roll() {
+		faceValue = rand.nextInt(5) + 1;
+	}
+	
+	/**
+	 * Returns the face value of the die
+	 * 
+	 * @return the face value of the die
+	 */
+	public int getFaceValue()
+	{
+		return faceValue;
+	}
 }
--- src/test/java/DieTest.java
+++ src/test/java/DieTest.java
@@ -1,5 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-class DieTest {
+import org.junit.jupiter.api.Test;
 
+class DieTest {
+	@Test
+	void ADieShouldRollANumberFromOneToSix() {
+		Die d = new Die();
+		for(int i = 0; i < 1000; ++i) {
+			d.roll();
+			assertTrue((d.getFaceValue() >= 1 && d.getFaceValue() <= 6));
+		}
+	}
 }
