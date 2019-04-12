55a85ed2fc82c16482bcee1e7082cbd1b2f9c76d
Jael Dubey <jael.dubey@heig-vd.ch>
2019-04-12 14:16:56 +0200

    Implémentation de Player, fixed #7

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,27 @@
 public class Player {
+    private String name;
+    private Piece piece;
+    private Die[] dice;
+    private Board board;
+    private int randomNumber;
+
+    public Player(String name, Piece piece, Board board) {
+        this.name = name;
+        this.piece = piece;
+        this.board = board;
+
+        dice = new Die[2];
+    }
+
+    public void takeTurn() {
+        for (Die die : dice) {
+            die.roll();
+            randomNumber += die.getFaceValue();
+        }
+
+        Square oldLoc = piece.getLocation();
+        Square newLoc = board.setLocation(oldLoc, randomNumber);
+
+        piece.setLocation(randomNumber);
+    }
 }
