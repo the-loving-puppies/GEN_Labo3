f884ce1ffc2e02503430d20ec2b4c85544bbc445
Jael Dubey <jael.dubey@heig-vd.ch>
2019-04-12 15:28:59 +0200

    Correction Player, Ajout test piece, Ajout classe FakeDice

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -1,6 +1,10 @@
 public class Piece {
     private Square location;
 
+    public Piece(Square location) {
+        this.location = location;
+    }
+
     public Square getLocation() {
         return location;
     }
