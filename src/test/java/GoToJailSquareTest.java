a76f65f24c1f101c233b0588fe0d2bdaf40db185
Jorge <jorge.fulgencioesteves@heig-vd.ch>
2019-05-03 15:24:32 +0200

    GoToJailSquareTest implementation

---
+++ src/test/java/GoToJailSquareTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GoToJailSquareTest {
+
+    private class FakeCup extends Cup {
+        public FakeCup() {
+            super(new LinkedList<Die>());
+        }
+
+        @Override
+        public int getTotal() {
+            return 3;
+        }
+    }
+
+    @Test
+    void checkGoToJail(){
+        Board board = new Board();
+        FakeCup fakeCup = new FakeCup();
+        Player player = new Player("Rob", fakeCup, board);
+
+        Square square = board.getSquare(2);
+
+        player.takeTurn();
+
+        assertEquals(square, player.getLocation());
+    }
+}
